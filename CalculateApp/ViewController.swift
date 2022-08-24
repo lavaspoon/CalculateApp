559d63ee86c08ef8e230767e5763c8a10b622a03
lavaspoon <lavaspoon@naver.com>
2022-08-25 00:33:19 +0900

    버튼 링크 구현

--- CalculateApp/ViewController.swift
+++ CalculateApp/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
+enum Operation {
+    case Plus
+    case Minus
+    case Divide
+    case Gop
+    case unknown
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperand: Operation = .unknown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func tapNumberBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapClearBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapDotBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapDivideBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapGopBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapMinusBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapPlusBtn(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapResultBtn(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
